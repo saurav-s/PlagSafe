cf18f0db405bdedf430efc3ac009cd2c8bcc890e
tridivnandi <nandi.t@husky.neu.edu>
2018-03-02 09:40:29 -0500

    Adding the functionality of parsing a python file to it's AST along with the unit testcases

---
+++ src/main/java/com/phasec/plagsafe/antlr/ASTPrinter.java
@@ -0,0 +1,62 @@
+package com.phasec.plagsafe.antlr;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.RuleContext;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import com.phasec.plagsafe.antlr.generated.Python3Parser;
+
+/**
+ * Responsible for printing an AST
+ * Referred from: Federico Tomassetti's blog 
+ * URL: https://tomassetti.me/parsing-any-language-in-java-in-5-minutes-using-antlr-
+ * for-example-python/
+ * @author Tridiv
+ *
+ */
+public class ASTPrinter {
+	
+	// member variable
+	private boolean ignoringWrappers = true;
+	
+	/**
+	 * setter for the member variable
+	 * @param ignoringWrappers
+	 */
+    public void setIgnoringWrappers(boolean ignoringWrappers) {
+        this.ignoringWrappers = ignoringWrappers;
+    }
+
+    /**
+     * Responsible for printing the RuleContext
+     * @param ctx
+     */
+    public void print(RuleContext ctx) {
+        explore(ctx, 0);
+    }
+
+    /**
+     * Helper method responsible for printing the rule context
+     * @param ctx
+     * @param indentation
+     */
+    private void explore(RuleContext ctx, int indentation) {
+        boolean toBeIgnored = ignoringWrappers
+                && ctx.getChildCount() == 1
+                && ctx.getChild(0) instanceof ParserRuleContext;
+        if (!toBeIgnored) {
+            String ruleName = Python3Parser.ruleNames[ctx.getRuleIndex()];
+            for (int i = 0; i < indentation; i++) {
+                System.out.print("  ");
+            }
+            System.out.println(ruleName);
+        }
+        for (int i=0;i<ctx.getChildCount();i++) {
+            ParseTree element = ctx.getChild(i);
+            if (element instanceof RuleContext) {
+                explore((RuleContext)element, indentation + (toBeIgnored ? 0 : 1));
+            }
+        }
+    }
+
+}
---
+++ src/main/java/com/phasec/plagsafe/antlr/generated/Python3Listener.java
@@ -0,0 +1,259 @@
+package com.phasec.plagsafe.antlr.generated;
+
+// Generated from Python3.g4 by ANTLR 4.0
+import org.antlr.v4.runtime.tree.*;
+import org.antlr.v4.runtime.Token;
+
+public interface Python3Listener extends ParseTreeListener {
+	void enterTestlist(Python3Parser.TestlistContext ctx);
+	void exitTestlist(Python3Parser.TestlistContext ctx);
+
+	void enterAssert_stmt(Python3Parser.Assert_stmtContext ctx);
+	void exitAssert_stmt(Python3Parser.Assert_stmtContext ctx);
+
+	void enterArgument(Python3Parser.ArgumentContext ctx);
+	void exitArgument(Python3Parser.ArgumentContext ctx);
+
+	void enterNot_test(Python3Parser.Not_testContext ctx);
+	void exitNot_test(Python3Parser.Not_testContext ctx);
+
+	void enterFile_input(Python3Parser.File_inputContext ctx);
+	void exitFile_input(Python3Parser.File_inputContext ctx);
+
+	void enterXor_expr(Python3Parser.Xor_exprContext ctx);
+	void exitXor_expr(Python3Parser.Xor_exprContext ctx);
+
+	void enterInteger(Python3Parser.IntegerContext ctx);
+	void exitInteger(Python3Parser.IntegerContext ctx);
+
+	void enterImport_from(Python3Parser.Import_fromContext ctx);
+	void exitImport_from(Python3Parser.Import_fromContext ctx);
+
+	void enterSingle_input(Python3Parser.Single_inputContext ctx);
+	void exitSingle_input(Python3Parser.Single_inputContext ctx);
+
+	void enterDecorated(Python3Parser.DecoratedContext ctx);
+	void exitDecorated(Python3Parser.DecoratedContext ctx);
+
+	void enterWith_item(Python3Parser.With_itemContext ctx);
+	void exitWith_item(Python3Parser.With_itemContext ctx);
+
+	void enterRaise_stmt(Python3Parser.Raise_stmtContext ctx);
+	void exitRaise_stmt(Python3Parser.Raise_stmtContext ctx);
+
+	void enterImport_as_name(Python3Parser.Import_as_nameContext ctx);
+	void exitImport_as_name(Python3Parser.Import_as_nameContext ctx);
+
+	void enterExcept_clause(Python3Parser.Except_clauseContext ctx);
+	void exitExcept_clause(Python3Parser.Except_clauseContext ctx);
+
+	void enterCompound_stmt(Python3Parser.Compound_stmtContext ctx);
+	void exitCompound_stmt(Python3Parser.Compound_stmtContext ctx);
+
+	void enterNumber(Python3Parser.NumberContext ctx);
+	void exitNumber(Python3Parser.NumberContext ctx);
+
+	void enterAnd_expr(Python3Parser.And_exprContext ctx);
+	void exitAnd_expr(Python3Parser.And_exprContext ctx);
+
+	void enterLambdef_nocond(Python3Parser.Lambdef_nocondContext ctx);
+	void exitLambdef_nocond(Python3Parser.Lambdef_nocondContext ctx);
+
+	void enterDictorsetmaker(Python3Parser.DictorsetmakerContext ctx);
+	void exitDictorsetmaker(Python3Parser.DictorsetmakerContext ctx);
+
+	void enterReturn_stmt(Python3Parser.Return_stmtContext ctx);
+	void exitReturn_stmt(Python3Parser.Return_stmtContext ctx);
+
+	void enterDotted_name(Python3Parser.Dotted_nameContext ctx);
+	void exitDotted_name(Python3Parser.Dotted_nameContext ctx);
+
+	void enterFlow_stmt(Python3Parser.Flow_stmtContext ctx);
+	void exitFlow_stmt(Python3Parser.Flow_stmtContext ctx);
+
+	void enterWhile_stmt(Python3Parser.While_stmtContext ctx);
+	void exitWhile_stmt(Python3Parser.While_stmtContext ctx);
+
+	void enterOr_test(Python3Parser.Or_testContext ctx);
+	void exitOr_test(Python3Parser.Or_testContext ctx);
+
+	void enterComparison(Python3Parser.ComparisonContext ctx);
+	void exitComparison(Python3Parser.ComparisonContext ctx);
+
+	void enterTest(Python3Parser.TestContext ctx);
+	void exitTest(Python3Parser.TestContext ctx);
+
+	void enterSubscript(Python3Parser.SubscriptContext ctx);
+	void exitSubscript(Python3Parser.SubscriptContext ctx);
+
+	void enterComp_for(Python3Parser.Comp_forContext ctx);
+	void exitComp_for(Python3Parser.Comp_forContext ctx);
+
+	void enterYield_arg(Python3Parser.Yield_argContext ctx);
+	void exitYield_arg(Python3Parser.Yield_argContext ctx);
+
+	void enterYield_expr(Python3Parser.Yield_exprContext ctx);
+	void exitYield_expr(Python3Parser.Yield_exprContext ctx);
+
+	void enterImport_stmt(Python3Parser.Import_stmtContext ctx);
+	void exitImport_stmt(Python3Parser.Import_stmtContext ctx);
+
+	void enterShift_expr(Python3Parser.Shift_exprContext ctx);
+	void exitShift_expr(Python3Parser.Shift_exprContext ctx);
+
+	void enterLambdef(Python3Parser.LambdefContext ctx);
+	void exitLambdef(Python3Parser.LambdefContext ctx);
+
+	void enterAnd_test(Python3Parser.And_testContext ctx);
+	void exitAnd_test(Python3Parser.And_testContext ctx);
+
+	void enterGlobal_stmt(Python3Parser.Global_stmtContext ctx);
+	void exitGlobal_stmt(Python3Parser.Global_stmtContext ctx);
+
+	void enterImport_as_names(Python3Parser.Import_as_namesContext ctx);
+	void exitImport_as_names(Python3Parser.Import_as_namesContext ctx);
+
+	void enterDecorators(Python3Parser.DecoratorsContext ctx);
+	void exitDecorators(Python3Parser.DecoratorsContext ctx);
+
+	void enterTry_stmt(Python3Parser.Try_stmtContext ctx);
+	void exitTry_stmt(Python3Parser.Try_stmtContext ctx);
+
+	void enterComp_op(Python3Parser.Comp_opContext ctx);
+	void exitComp_op(Python3Parser.Comp_opContext ctx);
+
+	void enterStar_expr(Python3Parser.Star_exprContext ctx);
+	void exitStar_expr(Python3Parser.Star_exprContext ctx);
+
+	void enterBreak_stmt(Python3Parser.Break_stmtContext ctx);
+	void exitBreak_stmt(Python3Parser.Break_stmtContext ctx);
+
+	void enterParameters(Python3Parser.ParametersContext ctx);
+	void exitParameters(Python3Parser.ParametersContext ctx);
+
+	void enterDecorator(Python3Parser.DecoratorContext ctx);
+	void exitDecorator(Python3Parser.DecoratorContext ctx);
+
+	void enterTfpdef(Python3Parser.TfpdefContext ctx);
+	void exitTfpdef(Python3Parser.TfpdefContext ctx);
+
+	void enterString(Python3Parser.StringContext ctx);
+	void exitString(Python3Parser.StringContext ctx);
+
+	void enterTestlist_comp(Python3Parser.Testlist_compContext ctx);
+	void exitTestlist_comp(Python3Parser.Testlist_compContext ctx);
+
+	void enterIf_stmt(Python3Parser.If_stmtContext ctx);
+	void exitIf_stmt(Python3Parser.If_stmtContext ctx);
+
+	void enterWith_stmt(Python3Parser.With_stmtContext ctx);
+	void exitWith_stmt(Python3Parser.With_stmtContext ctx);
+
+	void enterClassdef(Python3Parser.ClassdefContext ctx);
+	void exitClassdef(Python3Parser.ClassdefContext ctx);
+
+	void enterExprlist(Python3Parser.ExprlistContext ctx);
+	void exitExprlist(Python3Parser.ExprlistContext ctx);
+
+	void enterSmall_stmt(Python3Parser.Small_stmtContext ctx);
+	void exitSmall_stmt(Python3Parser.Small_stmtContext ctx);
+
+	void enterTrailer(Python3Parser.TrailerContext ctx);
+	void exitTrailer(Python3Parser.TrailerContext ctx);
+
+	void enterDotted_as_names(Python3Parser.Dotted_as_namesContext ctx);
+	void exitDotted_as_names(Python3Parser.Dotted_as_namesContext ctx);
+
+	void enterArith_expr(Python3Parser.Arith_exprContext ctx);
+	void exitArith_expr(Python3Parser.Arith_exprContext ctx);
+
+	void enterArglist(Python3Parser.ArglistContext ctx);
+	void exitArglist(Python3Parser.ArglistContext ctx);
+
+	void enterSimple_stmt(Python3Parser.Simple_stmtContext ctx);
+	void exitSimple_stmt(Python3Parser.Simple_stmtContext ctx);
+
+	void enterTypedargslist(Python3Parser.TypedargslistContext ctx);
+	void exitTypedargslist(Python3Parser.TypedargslistContext ctx);
+
+	void enterExpr(Python3Parser.ExprContext ctx);
+	void exitExpr(Python3Parser.ExprContext ctx);
+
+	void enterTerm(Python3Parser.TermContext ctx);
+	void exitTerm(Python3Parser.TermContext ctx);
+
+	void enterPower(Python3Parser.PowerContext ctx);
+	void exitPower(Python3Parser.PowerContext ctx);
+
+	void enterDotted_as_name(Python3Parser.Dotted_as_nameContext ctx);
+	void exitDotted_as_name(Python3Parser.Dotted_as_nameContext ctx);
+
+	void enterFactor(Python3Parser.FactorContext ctx);
+	void exitFactor(Python3Parser.FactorContext ctx);
+
+	void enterSliceop(Python3Parser.SliceopContext ctx);
+	void exitSliceop(Python3Parser.SliceopContext ctx);
+
+	void enterFuncdef(Python3Parser.FuncdefContext ctx);
+	void exitFuncdef(Python3Parser.FuncdefContext ctx);
+
+	void enterSubscriptlist(Python3Parser.SubscriptlistContext ctx);
+	void exitSubscriptlist(Python3Parser.SubscriptlistContext ctx);
+
+	void enterTest_nocond(Python3Parser.Test_nocondContext ctx);
+	void exitTest_nocond(Python3Parser.Test_nocondContext ctx);
+
+	void enterComp_iter(Python3Parser.Comp_iterContext ctx);
+	void exitComp_iter(Python3Parser.Comp_iterContext ctx);
+
+	void enterNonlocal_stmt(Python3Parser.Nonlocal_stmtContext ctx);
+	void exitNonlocal_stmt(Python3Parser.Nonlocal_stmtContext ctx);
+
+	void enterEval_input(Python3Parser.Eval_inputContext ctx);
+	void exitEval_input(Python3Parser.Eval_inputContext ctx);
+
+	void enterVfpdef(Python3Parser.VfpdefContext ctx);
+	void exitVfpdef(Python3Parser.VfpdefContext ctx);
+
+	void enterImport_name(Python3Parser.Import_nameContext ctx);
+	void exitImport_name(Python3Parser.Import_nameContext ctx);
+
+	void enterComp_if(Python3Parser.Comp_ifContext ctx);
+	void exitComp_if(Python3Parser.Comp_ifContext ctx);
+
+	void enterAugassign(Python3Parser.AugassignContext ctx);
+	void exitAugassign(Python3Parser.AugassignContext ctx);
+
+	void enterPass_stmt(Python3Parser.Pass_stmtContext ctx);
+	void exitPass_stmt(Python3Parser.Pass_stmtContext ctx);
+
+	void enterExpr_stmt(Python3Parser.Expr_stmtContext ctx);
+	void exitExpr_stmt(Python3Parser.Expr_stmtContext ctx);
+
+	void enterYield_stmt(Python3Parser.Yield_stmtContext ctx);
+	void exitYield_stmt(Python3Parser.Yield_stmtContext ctx);
+
+	void enterSuite(Python3Parser.SuiteContext ctx);
+	void exitSuite(Python3Parser.SuiteContext ctx);
+
+	void enterContinue_stmt(Python3Parser.Continue_stmtContext ctx);
+	void exitContinue_stmt(Python3Parser.Continue_stmtContext ctx);
+
+	void enterTestlist_star_expr(Python3Parser.Testlist_star_exprContext ctx);
+	void exitTestlist_star_expr(Python3Parser.Testlist_star_exprContext ctx);
+
+	void enterVarargslist(Python3Parser.VarargslistContext ctx);
+	void exitVarargslist(Python3Parser.VarargslistContext ctx);
+
+	void enterFor_stmt(Python3Parser.For_stmtContext ctx);
+	void exitFor_stmt(Python3Parser.For_stmtContext ctx);
+
+	void enterDel_stmt(Python3Parser.Del_stmtContext ctx);
+	void exitDel_stmt(Python3Parser.Del_stmtContext ctx);
+
+	void enterAtom(Python3Parser.AtomContext ctx);
+	void exitAtom(Python3Parser.AtomContext ctx);
+
+	void enterStmt(Python3Parser.StmtContext ctx);
+	void exitStmt(Python3Parser.StmtContext ctx);
+}
