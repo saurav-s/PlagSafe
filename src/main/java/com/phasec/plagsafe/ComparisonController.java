854ce07fa46c022dac568ddc32b6c992a5882624
Rohit Raj <raj.r@husky.neu.edu>
2018-03-21 23:10:46 -0400

    CS109-28 updating controller to expect multipart file data

--- src/main/java/com/phasec/plagsafe/ComparisonController.java
+++ src/main/java/com/phasec/plagsafe/ComparisonController.java
@@ -1,4 +1,16 @@
 package com.phasec.plagsafe;
 
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+
+@RestController
 public class ComparisonController {
+
+    @RequestMapping(value="/comparison", method= RequestMethod.POST)
+    public void process(ModelMap model, @RequestParam("file") MultipartFile[] submissions) throws Exception {
+        System.out.println(submissions);
+    }
 }
