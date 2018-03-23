f985a7f5d25c77484a14c2360073eea7556a883f
Rohit Raj <raj.r@husky.neu.edu>
2018-03-23 15:11:51 -0400

    CS109-28 calling all the strategies

--- src/main/java/com/phasec/plagsafe/detector/SubmissionCompare.java
+++ src/main/java/com/phasec/plagsafe/detector/SubmissionCompare.java
@@ -13,12 +13,13 @@ public class SubmissionCompare implements SubmissionComparable {
 
         //compare for variable/function renaming using Levenshtein distance
         context = new ComparisonContext(new RenamingDetectionStrategy());
-
-        //matchReportList.addAll(context.compare(submission1, submission2));
+        matchReportList.addAll(context.compare(submission1, submission2));
 
         // compare for similarity in code logic using AST of each file in the submission
         context = new ComparisonContext(new LogicalSimilarityDetectionStrategy());
-        context.compare(submission1, submission2);
+        matchReportList.addAll(context.compare(submission1, submission2));
+
+        context = new ComparisonContext(new RefactoringDetectionStrategy());
         matchReportList.addAll(context.compare(submission1, submission2));
 
         return matchReportList;
