9591a6c4f929884fd3c7b8a24d6ab1091c571e66
tridivnandi <nandi.t@husky.neu.edu>
2018-03-23 18:03:16 -0400

    CS109-28 Adding test cases for utilities

---
+++ src/test/java/UtilTest.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import util.LevenshteinDistanceGeneratorUtility;
+import util.NGramGeneratorUtility;
+
+public class UtilTest {
+	
+
+	
+	@Test
+	public void nGramGeneratorUtilityTest(){
+		
+		NGramGeneratorUtility object = new NGramGeneratorUtility();
+		List<String> res = object.getNGramList("Northeastern University", 3);
+		assertNotNull(res);
+		
+	}
+	
+	@Test
+	public void levenshteinDistanceGeneratorUtilityTest(){
+		
+		LevenshteinDistanceGeneratorUtility object = new LevenshteinDistanceGeneratorUtility();
+		int lDistance = object.getLevenshteinDistance("pale", "bale");
+		assertEquals(1, lDistance);
+		
+	}
+	
+
+}
