167a12eeddd614fdb493f671e562c9b62d9ea070
tridivnandi <nandi.t@husky.neu.edu>
2018-03-23 21:20:49 -0400

    CS109-28 Adding more unit tests for ComparisonService class

--- src/test/java/com/phasec/plagsafe/detector/ComparisonServiceTests.java
+++ src/test/java/com/phasec/plagsafe/detector/ComparisonServiceTests.java
@@ -1,5 +1,7 @@
 package com.phasec.plagsafe.detector;
 
+import static org.junit.Assert.*;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +11,8 @@ import org.junit.Test;
 
 import com.phasec.plagsafe.ComparisonService;
 import com.phasec.plagsafe.objects.FileModel;
+import com.phasec.plagsafe.objects.FileRecord;
+import com.phasec.plagsafe.objects.Report;
 import com.phasec.plagsafe.objects.SubmissionRecord;
 
 /**
@@ -16,6 +20,7 @@ import com.phasec.plagsafe.objects.SubmissionRecord;
  *
  */
 public class ComparisonServiceTests {
+	
     // this test is to initialize with single submission
     @Test
     public void testSingleSubmissionInitialization() {
@@ -32,5 +37,31 @@ public class ComparisonServiceTests {
         ComparisonService controllerForTest = new ComparisonService();
         controllerForTest.submissionStub(submissions);
         Assert.assertEquals(true, true);
+    }
+    
+    @Test
+    public void testRunComparisionForFiles(){
+    	
+    	File file1 = new File("resources/simple1.py");
+    	List<File> fileList1 = new ArrayList<>();
+    	fileList1.add(file1);
+    	FileRecord fr1 = new FileRecord(fileList1);
+    	
+    	File file2 = new File("resources/simple.py");
+    	List<File> fileList2 = new ArrayList<>();
+    	fileList2.add(file2);
+    	FileRecord fr2 = new FileRecord(fileList2);
+    	
+    	List<FileRecord> frList = new ArrayList<>();
+    	frList.add(fr1);
+    	frList.add(fr2);
+    	
+    	ComparisonService cs = new ComparisonService();
+    	List<Report> res = cs.runComparisionForFiles(frList);
+    	
+    	assertNotNull(res);
+    	assertEquals(res.size(), 3);
+    	
+    	
     }
 }
