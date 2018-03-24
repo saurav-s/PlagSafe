79255bf13178190b10aeffddf3ca171d7d63d2b8
Rohit Raj <raj.r@husky.neu.edu>
2018-03-23 22:15:40 -0400

    CS109-28 refactoring tests

---
+++ src/test/java/com/phasec/plagsafe/objects/SubmissibleRecordTests.java
@@ -0,0 +1,17 @@
+package com.phasec.plagsafe.objects;
+
+import com.phasec.plagsafe.detector.Submissible;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubmissibleRecordTests {
+    @Test
+    public void testSubmissibleRecordCreation() {
+        SubmissibleRecord record = new SubmissibleRecord(new ArrayList<>());
+        List<Submissible> expectedList = record.getSubmissibles();
+        Assert.assertEquals(expectedList.size(), 0);
+    }
+}
