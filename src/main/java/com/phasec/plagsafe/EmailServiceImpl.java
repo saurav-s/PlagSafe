a2a48d88d5d228c39d33011513e84575882eea58
Sanket Saurav <saurav.s@husky.neu.edu>
2018-03-28 23:47:50 -0400

    CS109-71 Abstracting email service

---
+++ src/main/java/com/phasec/plagsafe/EmailServiceImpl.java
@@ -0,0 +1,52 @@
+package com.phasec.plagsafe;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+/**
+ * Provides a mechanism to send emails 
+ * @author sanketsaurav
+ *
+ */
+@Service
+public class EmailServiceImpl implements EmailService {
+
+	@Autowired
+	private JavaMailSender javaMailSender;
+
+	@Value("#{'${list.of.admin.email}'.split(',')}")
+	private List<String> mailingList;
+
+	/**
+	 * 
+	 * @param toEmail
+	 * @param subject
+	 * @param message
+	 */
+	public void sendMail(String toEmail, String subject, String message) {
+		SimpleMailMessage mailMessage = new SimpleMailMessage();
+		mailMessage.setTo(toEmail);
+		mailMessage.setSubject(subject);
+		mailMessage.setText(message);
+		javaMailSender.send(mailMessage);
+	}
+
+	/**
+	 * Send Alert message to all the admin emails
+	 * @param message
+	 */
+	public void sendAlertMailToAdmins(String message) {
+		SimpleMailMessage mailMessage = new SimpleMailMessage();
+		mailMessage.setCc(mailingList.toArray(new String[0]));
+		mailMessage.setSubject("Team 109 - PlagSafe Application Alert");
+		mailMessage.setText(message);
+		javaMailSender.send(mailMessage);
+
+	}
+
+}
