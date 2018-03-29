a2a40f475996024c8d692dd259a96c3fc0b53d5f
Sanket Saurav <saurav.s@husky.neu.edu>
2018-03-28 23:51:49 -0400

    CS109-71 Abstracting comparison service and updating tests

--- src/main/java/com/phasec/plagsafe/ComparisonService.java
+++ src/main/java/com/phasec/plagsafe/ComparisonService.java
@@ -1,55 +1,31 @@
 package com.phasec.plagsafe;
-/**
- *
- */
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-
-import com.phasec.plagsafe.detector.DetectionEngine;
-import com.phasec.plagsafe.detector.Engine;
 import com.phasec.plagsafe.objects.FileRecord;
 import com.phasec.plagsafe.objects.Report;
 import com.phasec.plagsafe.objects.SubmissionRecord;
 
-import util.FileUtility;
-
 /**
- * Service for starting comparison of files
+ * Interface for comparing files
  * @author sanketsaurav
  *
  */
-@Service
-public class ComparisonService {
+public interface ComparisonService {
 
     /**
-     *
+     * Runs comparison for the SubmissionRecord list and generates a List of report containing match results
      * @param submissions
      * @param comparisonStrategy 
+     * @return List of report containing match results
      */
-    public List<Report> submissionStub(List<SubmissionRecord> submissions, StrategyType comparisonStrategy) {
-        Engine detectionEngine = new DetectionEngine();
-        return detectionEngine.runDetection(submissions, comparisonStrategy);
-    }
+    List<Report> submissionStub(List<SubmissionRecord> submissions, StrategyType comparisonStrategy);
     
     /**
-     * 
+     * Runs comparison for the file record list and generates a List of report containing match results
      * @param filesList
      * @param comparisonStrategy 
-     * @return
+     * @return List of report containing match results
      */
-    public List<Report> runComparisionForFiles(List<FileRecord> filesList, StrategyType comparisonStrategy){
-		List<SubmissionRecord>  submissionRecords = new ArrayList<>();
-		for(FileRecord files : filesList) {
-			SubmissionRecord record = FileUtility.getFileMapList(files);
-			submissionRecords.add(record);
-		}
-		List<Report> reports = submissionStub(submissionRecords, comparisonStrategy);
-		Collections.sort(reports);
-		return reports;
-		
-    }
+    List<Report> runComparisionForFiles(List<FileRecord> filesList, StrategyType comparisonStrategy);
 }
--- src/test/java/com/phasec/plagsafe/detector/ComparisonServiceTests.java
+++ src/test/java/com/phasec/plagsafe/detector/ComparisonServiceTests.java
@@ -1,6 +1,7 @@
 package com.phasec.plagsafe.detector;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -8,61 +9,75 @@ import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.phasec.plagsafe.ComparisonService;
+import com.phasec.plagsafe.PlagsafeApplication;
 import com.phasec.plagsafe.StrategyType;
 import com.phasec.plagsafe.objects.FileModel;
 import com.phasec.plagsafe.objects.FileRecord;
 import com.phasec.plagsafe.objects.Report;
 import com.phasec.plagsafe.objects.SubmissionRecord;
 
+
 /**
  * Tests for the Comparison Controller class
  *
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes =  PlagsafeApplication.class)
 public class ComparisonServiceTests {
-	
-    // this test is to initialize with single submission
-    @Test
-    public void testSingleSubmissionInitialization() {
-        List<SubmissionRecord> submissions = new ArrayList<>();
 
-        FileModel file = new FileModel();
-        file.setFileName("simple.py");
-        file.setFileData(new File("resources/simple.py"));
-        SubmissionRecord record = new SubmissionRecord();
-        List<FileModel> fileList = new ArrayList<FileModel>();
-        fileList.add(file);
-        record.setFiles(fileList);
-        submissions.add(record);
-        ComparisonService controllerForTest = new ComparisonService();
-        controllerForTest.submissionStub(submissions,StrategyType.ALL);
-        Assert.assertEquals(true, true);
-    }
-    
-    @Test
-    public void testRunComparisionForFiles(){
-    	
-    	File file1 = new File("resources/simple1.py");
-    	List<File> fileList1 = new ArrayList<>();
-    	fileList1.add(file1);
-    	FileRecord fr1 = new FileRecord(fileList1);
-    	
-    	File file2 = new File("resources/simple.py");
-    	List<File> fileList2 = new ArrayList<>();
-    	fileList2.add(file2);
-    	FileRecord fr2 = new FileRecord(fileList2);
-    	
-    	List<FileRecord> frList = new ArrayList<>();
-    	frList.add(fr1);
-    	frList.add(fr2);
-    	
-    	ComparisonService cs = new ComparisonService();
-    	List<Report> res = cs.runComparisionForFiles(frList,StrategyType.ALL);
-    	
-    	assertNotNull(res);
-    	assertEquals(res.size(), 3);
-    	
-    	
-    }
+	@Autowired
+	ComparisonService comparisonService;
+
+
+
+	/**
+	 *  this test is to initialize with single submission
+	 */
+	@Test
+	public void testSingleSubmissionInitialization() {
+		List<SubmissionRecord> submissions = new ArrayList<>();
+
+		FileModel file = new FileModel();
+		file.setFileName("simple.py");
+		file.setFileData(new File("resources/simple.py"));
+		SubmissionRecord record = new SubmissionRecord();
+		List<FileModel> fileList = new ArrayList<FileModel>();
+		fileList.add(file);
+		record.setFiles(fileList);
+		submissions.add(record);
+		comparisonService.submissionStub(submissions, StrategyType.ALL);
+		Assert.assertEquals(true, true);
+	}
+
+
+
+	@Test
+	public void testRunComparisionForFiles() {
+
+		File file1 = new File("resources/simple1.py");
+		List<File> fileList1 = new ArrayList<>();
+		fileList1.add(file1);
+		FileRecord fr1 = new FileRecord(fileList1);
+
+		File file2 = new File("resources/simple.py");
+		List<File> fileList2 = new ArrayList<>();
+		fileList2.add(file2);
+		FileRecord fr2 = new FileRecord(fileList2);
+
+		List<FileRecord> frList = new ArrayList<>();
+		frList.add(fr1);
+		frList.add(fr2);
+
+		List<Report> res = comparisonService.runComparisionForFiles(frList, StrategyType.ALL);
+
+		assertNotNull(res);
+		assertEquals(res.size(), 3);
+
+	}
 }
