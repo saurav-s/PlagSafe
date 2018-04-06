d767d9c11c85f99e1ccaa74e8aee69b76623378e
Sanket Saurav <saurav.s@husky.neu.edu>
2018-04-06 17:10:23 -0400

    CS109-84 CS109-68 adding submisson service test

---
+++ src/test/java/com/phasec/plagsafe/ClassSubmissionServiceTests.java
@@ -0,0 +1,39 @@
+package com.phasec.plagsafe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.multipart.MultipartFile;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = PlagsafeApplication.class)
+public class ClassSubmissionServiceTests {
+
+	@Autowired
+	ClassSubmissionService submissionService;
+	
+   // public String initializeAndCompare(MultipartFile[] submissions, List<String> paths, String strategy)
+	@Test
+	public void testInitializeAndCompare() {
+		List<String> paths = new ArrayList<>();
+		paths.add( "test/team101/simple.py");
+		paths.add( "test/team102/simple.py");
+		
+		MockMultipartFile firstFile = new MockMultipartFile("uploadfile1", "simple.py", "text/plain",
+				"some xml".getBytes());
+		MockMultipartFile secondFile = new MockMultipartFile("uploadfile2", "simple.py"," text/plain",
+				"some other type".getBytes());
+		MultipartFile[] submissions = new MultipartFile[2];
+		submissions[0] = firstFile;
+		submissions[1] = secondFile;
+		String strategy = "ALL";
+		String initializeAndCompare = submissionService.initializeAndCompare(submissions, paths, strategy );
+	}
+
+}
