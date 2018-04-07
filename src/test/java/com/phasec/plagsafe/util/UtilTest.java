896c4a0f7344d81a81155a6d54550f9fd02a8e68
tridivnandi <nandi.t@husky.neu.edu>
2018-04-06 20:35:27 -0400

    Adding test cases for Utilities to cover more code

--- src/test/java/com/phasec/plagsafe/util/UtilTest.java
+++ src/test/java/com/phasec/plagsafe/util/UtilTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 
 import util.LevenshteinDistanceGeneratorUtility;
 import util.NGramGeneratorUtility;
+import util.WeightPropertyReader;
 
 public class UtilTest {
 	
@@ -28,6 +29,36 @@ public class UtilTest {
 		LevenshteinDistanceGeneratorUtility object = new LevenshteinDistanceGeneratorUtility();
 		int lDistance = object.getLevenshteinDistance("pale", "bale");
 		assertEquals(1, lDistance);
+		
+	}
+	
+	@Test
+	public void test_WeightPropertyReader_setRenaming_weight(){
+		WeightPropertyReader object = WeightPropertyReader.makeReaderObject();
+		object.setRenaming_weight(13);
+		
+		int obtainedRenamingWeight = object.getRenaming_weight();
+		assertEquals(obtainedRenamingWeight, 13);
+		
+	}
+	
+	@Test
+	public void test_WeightPropertyReader_setLogical_weight(){
+		WeightPropertyReader object = WeightPropertyReader.makeReaderObject();
+		object.setLogical_weight(33);
+		
+		int obtainedLogicalWeight = object.getLogical_weight();
+		assertEquals(obtainedLogicalWeight, 33);
+		
+	}
+	
+	@Test
+	public void test_WeightPropertyReader_setRefactoring_weight(){
+		WeightPropertyReader object = WeightPropertyReader.makeReaderObject();
+		object.setRefactoring_weight(23);
+		
+		int obtainedLogicalWeight = object.getRefactoring_weight();
+		assertEquals(obtainedLogicalWeight, 23);
 		
 	}
 	
