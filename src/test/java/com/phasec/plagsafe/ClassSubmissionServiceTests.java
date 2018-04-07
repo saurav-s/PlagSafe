4dd9313c125ac93b6dc685ac000396ed1b0984ac
Sanket Saurav <saurav.s@husky.neu.edu>
2018-04-06 22:08:36 -0400

    CS109-84 CS109-68 adding more tests for class submisson service

--- src/test/java/com/phasec/plagsafe/ClassSubmissionServiceTests.java
+++ src/test/java/com/phasec/plagsafe/ClassSubmissionServiceTests.java
@@ -1,5 +1,10 @@
 package com.phasec.plagsafe;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyListOf;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.when;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,39 +12,86 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.phasec.plagsafe.objects.FileRecord;
 import com.phasec.plagsafe.objects.Report;
 
+
+/**
+ * @author sanketsaurav
+ *
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = PlagsafeApplication.class)
 public class ClassSubmissionServiceTests {
 
 	@Autowired
 	ClassSubmissionService submissionService;
-	
-	
-   // public String initializeAndCompare(MultipartFile[] submissions, List<String> paths, String strategy)
+
+	@MockBean
+	ComparisonService comparisonService;
+
+
+
+	/**
+	 * Test for initialize and compare
+	 */
 	@Test
 	public void testInitializeAndCompare() {
+		Report report = new Report("test/team101/simple.py", "test/team102/simple.py", 100, "test_Remarks");
+		List<Report> reports = new ArrayList<>();
+		reports.add(report);
+		when(comparisonService.runComparisionForFiles(anyListOf(FileRecord.class), eq(StrategyType.ALL)))
+				.thenReturn(reports);
 		List<String> paths = new ArrayList<>();
-		paths.add( "test/team101/simple.py");
-		paths.add( "test/team102/simple.py");
-		
-		
+		paths.add("test/team101/simple.py");
+		paths.add("test/team102/simple.py");
+
 		MockMultipartFile firstFile = new MockMultipartFile("uploadfile1", "simple.py", "text/plain",
 				"some xml".getBytes());
-		MockMultipartFile secondFile = new MockMultipartFile("uploadfile2", "simple.py"," text/plain",
+		MockMultipartFile secondFile = new MockMultipartFile("uploadfile2", "simple.py", " text/plain",
 				"some other type".getBytes());
 		MultipartFile[] submissions = new MultipartFile[2];
 		submissions[0] = firstFile;
 		submissions[1] = secondFile;
 		String strategy = "ALL";
-		String initializeAndCompare = submissionService.initializeAndCompare(submissions, paths, strategy );
+		String responseMessage = submissionService.initializeAndCompare(submissions, paths, strategy);
+		assertEquals("[{\"sourceFile\":\"test/team101/simple.py\",\"targetFile\":\"test/team102/simple.py\",\"matchPercentage\":100,\"matchRemark\":\"test_Remarks\"}]", responseMessage);
+	}
+
+
+
+	/**
+	 * Test for initialize and compare fail
+	 */
+	@Test
+	public void testInitializeAndCompareFail() {
+
+		when(comparisonService.runComparisionForFiles(anyListOf(FileRecord.class), eq(StrategyType.ALL)))
+				.thenThrow(new RuntimeException());
+		List<String> paths = new ArrayList<>();
+		paths.add("test/team101/simple.py");
+		paths.add("test/team102/simple.py");
+
+		MockMultipartFile firstFile = new MockMultipartFile("uploadfile1", "simple.py", "text/plain",
+				"some xml".getBytes());
+		MockMultipartFile secondFile = new MockMultipartFile("uploadfile2", "simple.py", " text/plain",
+				"some other type".getBytes());
+		MultipartFile[] submissions = new MultipartFile[2];
+		submissions[0] = firstFile;
+		submissions[1] = secondFile;
+		String strategy = "ALL";
+		String responseMessage = submissionService.initializeAndCompare(submissions, paths, strategy);
+		assertEquals("\"Error occurred while uploading the file \"", responseMessage);
 	}
 	
+	/**
+	 * Test for reformat File names
+	 */
 	@Test
 	public void test_reformatFileNames(){
 		List<Report> mockReportList = new ArrayList<>();
@@ -49,13 +101,5 @@ public class ClassSubmissionServiceTests {
 		
 		
 	}
-	
-//	@Test
-//	public void test_makeRecordFiles(){
-//		
-//		List<String> mockSubmissionFile = new ArrayList<>();
-//		
-//		
-//	}
 
 }
